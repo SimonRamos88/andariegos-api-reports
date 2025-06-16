d7bc1c8ff0d4e197798f09935e6c5eb5905c0351
SimonRamos88 <joramos@unal.edu.co>
2025-06-15 19:41:20 -0500

    event id corregido

--- app/schemas/report_schema.py
+++ app/schemas/report_schema.py
@@ -2,7 +2,7 @@ from marshmallow import Schema, fields, ValidationError, validate
 
 class ReportSchema(Schema):
     _id = fields.Str()
-    id_events = fields.List(fields.Int(), required=True)
+    id_event = fields.Int(required=True)
     id_reporter = fields.Str(required=True)
     state = fields.Str(required=True, validate=validate.OneOf(['accepted', 'denied', 'pending']))
     description = fields.Str()
